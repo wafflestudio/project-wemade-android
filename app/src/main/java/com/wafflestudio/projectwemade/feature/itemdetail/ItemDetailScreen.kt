4c9c148e013330b3ed36a57da1da4a0aa325755d
wjshim2003 <wjshim@snu.ac.kr>
2023-11-23 21:00:09 +0900

    ItemDetailScreen

---
+++ app/src/main/java/com/wafflestudio/projectwemade/feature/itemdetail/ItemDetailScreen.kt
@@ -0,0 +1,115 @@
+package com.wafflestudio.projectwemade.feature.itemdetail
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.wafflestudio.projectwemade.R
+import com.wafflestudio.projectwemade.component.BottomBarButton
+import com.wafflestudio.projectwemade.component.BottomBar
+import com.wafflestudio.projectwemade.component.CenterTopBar
+import com.wafflestudio.projectwemade.component.TopBarButton
+
+@Composable
+fun ItemDetailScreen(
+    itemOptions: List<ItemOption>,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier.fillMaxSize()
+    ) {
+        CenterTopBar(
+            modifier = Modifier
+                .fillMaxWidth(),
+            title = "아이템 상세",
+            leftAction = {
+                TopBarButton(
+                    onClick = {},
+                    text = "뒤로가기"
+                )
+            },
+            rightAction = {
+                TopBarButton(
+                    onClick = {},
+                    text = "장바구니"
+                )
+            }
+        )
+
+        Column (
+            modifier = Modifier
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(text = "아이템 이름")
+            Text(text = "아이템 설명")
+            for(itemOption in itemOptions)
+                ItemOptionCard(itemOption)
+        }
+
+        BottomBar(
+            modifier = Modifier
+                .fillMaxWidth(),
+            leftComposable = {
+                Column (
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    BottomBarButton(
+                        onClick = {},
+                        text = "수량 선택"
+                    )
+                    BottomBarButton(
+                        onClick = { },
+                        text = "좋아요"
+                    )
+                }
+            },
+            rightComposable = {
+                Column (
+                    horizontalAlignment = Alignment.End
+                ) {
+                    BottomBarButton(
+                        onClick = {},
+                        text = "장바구니 담기"
+                    )
+                    BottomBarButton(
+                        onClick = { },
+                        text = "바로 주문"
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ItemDetailScreenPreview(){
+    ItemDetailScreen(
+        itemOptions = listOf(
+            ItemOption(
+                optionName = "온도",
+                options = listOf("hot", "cold")
+            ),
+            ItemOption(
+                optionName = "농도",
+                options = listOf<String>("기본", "진하게", "연하게")
+            )
+        )
+    )
+}
