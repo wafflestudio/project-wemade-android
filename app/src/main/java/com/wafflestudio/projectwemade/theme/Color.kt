ea99024da1d4a2d418103a61e555e9af04c2b106
eastshine2741 <eastshine@snu.ac.kr>
2023-11-26 11:01:16 +0900

    color 수정

--- app/src/main/java/com/wafflestudio/projectwemade/theme/Color.kt
+++ app/src/main/java/com/wafflestudio/projectwemade/theme/Color.kt
@@ -6,10 +6,15 @@ object WemadeColors {
     val White900 = Color(0xFFFFFFFF)
     val Black900 = Color(0xFF000000)
 
-    val Gray100 = Color(0xFFEBEBEB)
+    val DarkGray = Color(0xFFD9D9D9)
+    val MediumGray = Color(0xFF666666)
+    val LightGray = Color(0xFFE6E5E5)
+
     val Red50 = Color(0xFFFEE7E7)
     val Red200 = Color(0xFFEB9393)
     val Blue50 = Color(0xFFDDE8FA)
     val Blue200 = Color(0xFF738EBB)
     val Yellow50 = Color(0xFFFFF2D1)
+
+    val MainGreen = Color(0xFF50FF81)
 }
