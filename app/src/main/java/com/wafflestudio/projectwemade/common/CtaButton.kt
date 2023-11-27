913d4fe8fed5bb1e2dd8e4db57b945c1ea7f0709
eastshine2741 <eastshine@snu.ac.kr>
2023-11-27 13:48:09 +0900

    CtaButton 추가

---
+++ app/src/main/java/com/wafflestudio/projectwemade/common/CtaButton.kt
@@ -0,0 +1,67 @@
+package com.wafflestudio.projectwemade.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wafflestudio.projectwemade.theme.ProjectWemadeAndroidTheme
+import com.wafflestudio.projectwemade.theme.WemadeColors
+
+@Composable
+fun CtaButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    enabledColor: Color = MaterialTheme.colorScheme.primary,
+    disabledColor: Color = WemadeColors.LightGray,
+    enabledTextColor: Color = MaterialTheme.colorScheme.onPrimary,
+    disabledTextColor: Color = WemadeColors.MediumGray,
+) {
+    Row(
+        modifier = modifier
+            .then(
+                if (enabled) {
+                    Modifier.clickable { onClick() }
+                } else {
+                    Modifier
+                }
+            )
+            .background(
+                color = if (enabled) enabledColor else disabledColor,
+                shape = RoundedCornerShape(4.dp)
+            )
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = text,
+            color = if (enabled) enabledTextColor else disabledTextColor,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.titleLarge
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CtaButtonPreview() {
+    ProjectWemadeAndroidTheme {
+        CtaButton(
+            text = "주문하기",
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
--- app/src/main/java/com/wafflestudio/projectwemade/feature/home/HomeScreen.kt
+++ app/src/main/java/com/wafflestudio/projectwemade/feature/home/HomeScreen.kt
@@ -1,8 +1,8 @@
 package com.wafflestudio.projectwemade.feature.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
@@ -10,30 +10,48 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.wafflestudio.projectwemade.common.CtaButton
 import com.wafflestudio.projectwemade.common.LocalBottomSurfaceState
 import com.wafflestudio.projectwemade.theme.WemadeColors
 
 @Composable
 fun HomeScreen() {
     val bottomSurfaceState = LocalBottomSurfaceState.current
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(WemadeColors.Red50)
+            .background(WemadeColors.White900),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "Home screen",
             modifier = Modifier
-                .align(Alignment.Center)
-                .clickable {
-                    bottomSurfaceState.content = {
-                        Text(
-                            text = "hello!!!",
-                            modifier = Modifier.height(100.dp)
-                        )
-                    }
-                    bottomSurfaceState.visible = bottomSurfaceState.visible.not()
+        )
+        CtaButton(
+            text = "show bottomsurface",
+            onClick = {
+                bottomSurfaceState.content = {
+                    Text(
+                        text = "hello!!!",
+                        modifier = Modifier.height(100.dp)
+                    )
                 }
+                bottomSurfaceState.visible = bottomSurfaceState.visible.not()
+            }
+        )
+        CtaButton(
+            text = "disabled",
+            onClick = {
+                bottomSurfaceState.content = {
+                    Text(
+                        text = "hello!!!",
+                        modifier = Modifier.height(100.dp)
+                    )
+                }
+                bottomSurfaceState.visible = bottomSurfaceState.visible.not()
+            },
+            enabled = false,
         )
     }
 }
