d991d584a1d92a6b6ac97a466dbdbad647bc8a2a
wjshim2003 <wjshim@snu.ac.kr>
2023-12-03 09:47:43 +0900

    ItemDetailScreen 디자인 반영 (TODO: 뒤로가기, 좋아요 버튼)

--- app/src/main/java/com/wafflestudio/projectwemade/component/CenterTopBar.kt
+++ app/src/main/java/com/wafflestudio/projectwemade/component/CenterTopBar.kt
@@ -1,12 +1,11 @@
 package com.wafflestudio.projectwemade.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -52,7 +51,7 @@ fun CenterTopBar(
     Box(
         modifier = modifier
             .fillMaxWidth()
-//            .background(color = WemadeColors.Blue50)
+            .padding(vertical = 8.dp, horizontal = 21.dp)
     ) {
         Box(
             modifier = Modifier.align(Alignment.CenterStart)
@@ -62,7 +61,7 @@ fun CenterTopBar(
         Text(
             text = title,
             modifier = Modifier.align(Alignment.Center),
-            fontSize = 30.sp,
+            style = MaterialTheme.typography.titleLarge
         )
         Box(
             modifier = Modifier.align(Alignment.CenterEnd)
