57bd58058673246a072bb9759e876fce49a4ace7
wjshim2003 <wjshim@snu.ac.kr>
2023-12-10 17:38:14 +0900

    StartScreen 버튼 디자인 때문에 리소스 좀 편집함

--- app/src/main/java/com/wafflestudio/projectwemade/feature/login/StartScreen.kt
+++ app/src/main/java/com/wafflestudio/projectwemade/feature/login/StartScreen.kt
@@ -1,7 +1,66 @@
 package com.wafflestudio.projectwemade.feature.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.wafflestudio.projectwemade.NavigationRoutes
+import com.wafflestudio.projectwemade.common.LocalNavController
+import com.wafflestudio.projectwemade.component.BorderButton
+import com.wafflestudio.projectwemade.component.CtaButton
+import com.wafflestudio.projectwemade.icon.WhiteCup
+import com.wafflestudio.projectwemade.theme.WemadeColors
 
 @Composable
 fun StartScreen() {
+    val navController = LocalNavController.current
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(WemadeColors.MainGreen)
+            .padding(horizontal = 24.dp, vertical = 29.dp)
+    ) {
+        WhiteCup(
+            modifier = Modifier
+                .width(100.dp)
+                .height(180.dp)
+                .align(Alignment.Center)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            BorderButton(
+                text = "로그인",
+                onClick = {
+                    navController.navigate(NavigationRoutes.SIGN_IN)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                borderColor = Color(0x00ffffff),
+                fontWeight = FontWeight.Bold
+            )
+            CtaButton(
+                text = "회원가입",
+                onClick = {
+                    navController.navigate(NavigationRoutes.SIGN_UP)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                enabledTextColor = WemadeColors.White900,
+                borderColor = WemadeColors.White900
+            )
+        }
+    }
 }
