d2bfab127ddbbfbc628179d0aa5236639a57c71e
eastshine2741 <eastshine@snu.ac.kr>
2023-12-28 00:40:23 +0900

    SignUpScreen UI 수정

--- app/src/main/java/com/wafflestudio/projectwemade/feature/authentication/SignUpScreen.kt
+++ app/src/main/java/com/wafflestudio/projectwemade/feature/authentication/SignUpScreen.kt
@@ -3,7 +3,6 @@ package com.wafflestudio.projectwemade.feature.authentication
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -24,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.wafflestudio.projectwemade.R
@@ -44,17 +45,18 @@ fun SignUpScreen(
     var id by remember { mutableStateOf("") }
     var pw by remember { mutableStateOf("") }
     var pwCheck by remember { mutableStateOf("") }
+    var privacyChecked by remember { mutableStateOf(false) }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(WemadeColors.White900)
-            .padding(horizontal = 20.dp)
+            .padding(start = 20.dp, end = 20.dp, top = 60.dp)
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .align(Alignment.Center)
+                .weight(1f)
                 .verticalScroll(rememberScrollState())
         ) {
             LogoIcon(
@@ -71,14 +73,16 @@ fun SignUpScreen(
             ) {
                 Text(
                     text = "어서오세요, 그린텀블러입니다.",
+                    fontWeight = FontWeight.Bold,
                     style = MaterialTheme.typography.titleLarge
                 )
                 Text(
                     text = "주문 서비스 이용을 위해 회원가입해 주세요.",
+                    fontWeight = FontWeight.SemiBold,
                     style = MaterialTheme.typography.titleMedium
                 )
             }
-            Spacer(modifier = Modifier.height(60.dp))
+            Spacer(modifier = Modifier.height(40.dp))
             Column(
                 modifier = Modifier.align(Alignment.CenterHorizontally),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -103,8 +107,8 @@ fun SignUpScreen(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 30.dp)
-                .align(Alignment.BottomCenter),
+                .background(WemadeColors.White900)
+                .padding(bottom = 30.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
             Row(
@@ -112,54 +116,59 @@ fun SignUpScreen(
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 Checkbox(
-                    checked = false,
-                    onCheckChanged = {},
-                    modifier = Modifier.align(Alignment.Top)
+                    checked = privacyChecked,
+                    onCheckChanged = { privacyChecked = it },
+                    modifier = Modifier
+                        .align(Alignment.Top)
+                        .size(24.dp)
                 )
                 Column {
                     Text(
                         text = "개인정보 수집 및 이용약관 동의 [필수]",
+                        fontWeight = FontWeight.SemiBold,
                         style = MaterialTheme.typography.titleMedium
                     )
                     Text(
                         text = "이용약관을 동의하셔야 회원가입이 가능합니다.",
-                        style = MaterialTheme.typography.bodyLarge,
+                        style = MaterialTheme.typography.bodySmall,
                         color = WemadeColors.DarkGray
                     )
                 }
             }
             CtaButton(
-                text = "로그인",
+                text = "회원가입",
                 onClick = {
-                    authViewModel.signUp(
-                        username = id,
-                        password = pw,
-                        passwordConfirm = pwCheck,
-                        onUsernameDuplicated = {
-                            Toast.makeText(
-                                context,
-                                context.getString(R.string.error_duplicated_username),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        },
-                        onPasswordInsecure = {
-                            Toast.makeText(
-                                context,
-                                context.getString(R.string.error_insecure_password),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        },
-                        onPasswordTypo = {
-                            Toast.makeText(
-                                context,
-                                context.getString(R.string.error_password_typo),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        },
-                        onSuccess = {
-                            navController.popBackStack()
-                        }
-                    )
+                    if (privacyChecked) {
+                        authViewModel.signUp(
+                            username = id,
+                            password = pw,
+                            passwordConfirm = pwCheck,
+                            onUsernameDuplicated = {
+                                Toast.makeText(
+                                    context,
+                                    context.getString(R.string.error_duplicated_username),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            },
+                            onPasswordInsecure = {
+                                Toast.makeText(
+                                    context,
+                                    context.getString(R.string.error_insecure_password),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            },
+                            onPasswordTypo = {
+                                Toast.makeText(
+                                    context,
+                                    context.getString(R.string.error_password_typo),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            },
+                            onSuccess = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
                 },
                 modifier = Modifier.fillMaxWidth()
             )
